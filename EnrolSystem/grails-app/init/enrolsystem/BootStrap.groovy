7223859b14e099e65d114153d09b7f9b59173b42
ABC123Admin <abc123admin@ABC123DEBIAN01.ABC123>
2019-02-20 19:34:19 +0000

    Bootstrapping worksheet finished and working I think() exit

--- EnrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
+++ EnrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
@@ -1,8 +1,21 @@
-package enrolsystem
-
+//package enrolsystem
+package com.enrol
 class BootStrap {
 
     def init = { servletContext ->
+
+	def computing= new Course(
+	department: "department",
+	courseTitle: "Computing",
+	courseLeader: "Dr Michelle Murphy",
+	courseCode: "CS123",
+	startDate: new Date('09/09/2019'),
+	endDate: new Date('07/07/2023'),
+	description: "Loren ipsum dolor r sit ametsit amolo ipsum doloet ipsum dr sit amet",
+	numberOfStudents: 55,
+	tuitionFees: 9000.60,
+	studyMode: "Full Time").save()
+
     }
     def destroy = {
     }
